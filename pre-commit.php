bdf27e1c46fcb64e387698f13ae16742d2a9a1f7
Joseph Sutton <joe@c2cschools.com>
2014-07-10 11:28:58 -0500

    Test

--- pre-commit.php
+++ pre-commit.php
@@ -1,32 +1,84 @@
 <?php
 
-	$lines = [];
-	$status = 0;
-	$check = "";
-	$rc = 0;
+	$precommit = new precommit();
+
+	////////
+
+	class hook {
 	
-	$debug = [
-		"console.log",
-		"print_r",
-		"var_dump"
-	];
+		public function __construct() {
+			$this->execute();
+		}
+		
+		/**
+		 * To be overridden
+		 */
+		protected function execute() {
+		}
 	
-	exec('git rev-parse --verify HEAD 2> /dev/null', $lines, $rc);
+		protected function debug( $input, $return = false ) {
+		
+			$dtNow = new \DateTime("NOW", new \DateTimeZone("America/Chicago"));
+			
+			$debug = "[DEBUG] [";
+			$debug.= $dtNow->format("t") . "] :: ";
+			$debug.= $input;
+			$debug.= "\n";
+			
+			if( $return ) {
+				return $debug;
+			} else {
+				echo $debug;
+			}
+		
+		}
+		
+	};
+
+	class precommit extends hook {
 	
-	if( $rc ) {
-		$check = "4b825dc642cb6eb9a060e54bf8d69288fbee4904";
-	} else {
-		$check = "HEAD";
-	}
+		protected $lines = [];
 	
-	exec('git diff-index --cached --name-status '. $check, $lines);
-	
-	print_r($lines);
-	
-	foreach( $lines as $line ) {
-	
-		echo "line: ". $line;
-	
+		public function __construct() {
+			parent::__construct();
+		}
+		
+		protected function execute() {
+			
+			$lines = [];
+			$status = 0;
+			$check = "";
+			$rc = 0;
+			
+			$debug = [
+				"console.log",
+				"print_r",
+				"var_dump"
+			];
+			
+			exec('git rev-parse --verify HEAD 2> /dev/null', $lines, $rc);
+			
+			if( $rc ) {
+				$check = "4b825dc642cb6eb9a060e54bf8d69288fbee4904";
+			} else {
+				$check = "HEAD";
+			}
+			
+			exec('git diff-index --cached --name-status '. $check, $lines);
+			
+			print_r($lines);
+			
+			foreach( $lines as $line ) {
+			
+				$line = trim($line);
+				$line = preg_replace("(A|M)", "", $line);
+				
+				$this->debug( "File: " . $line );
+			
+			}
+		
+		}
+		
 	}
 	
 ?>
