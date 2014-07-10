5511cbb3925477b9d0deb689d3bffe7c3f539b98
Joseph Sutton <joe@c2cschools.com>
2014-07-10 10:57:40 -0500

    Test

--- hook.php
+++ hook.php
@@ -1,5 +1,20 @@
 <?php
 
-	echo "Hello world!";
+	$output = [];
+	$status = 0;
+	
+	$debug = [
+		"console.log",
+		"print_r",
+		"var_dump"
+	];
+	
+	exec('git diff --name-only', $lines);
+	
+	foreach( $lines as $line ) {
+	
+		echo $line;
+	
+	}
 	
 ?>
